01983deb8a3e548649fc83de0e6f124a475465f1
FrancescoMinichiello <fraps2278@gmail.com>
2024-11-05 17:38:05 +0100

    added seeder

--- database/migrations/2024_11_04_234124_create_projects_table.php
+++ database/migrations/2024_11_04_234124_create_projects_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string("title", 255);
+            $table->string("author", 255);
+            $table->text("content");
             $table->timestamps();
         });
     }
